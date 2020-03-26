49f87a612b3a828de373110a2154daf3847deb79
sicou2 <sicou2@gmail.com>
2020-03-25 20:53:19 -0500

    Started on Ch4, did not get super far

---
+++ python_work/part1/ch04/magicians.py
@@ -0,0 +1,6 @@
+magicians = ['alice', 'david', 'penn', 'teller', 'matt']
+for magician in magicians:
+    print(f'{magician.title()}, that hand work was smooth!')
+    print(f'I simply cannot wait for the next trick {magician.title()}!\n')
+
+print('I would like to thank everyone for coming out! That was a great show!')
