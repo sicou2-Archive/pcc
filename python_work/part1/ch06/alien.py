ff5a7ceee6cde454ae24287e6abf7f5f86a11a49
sicou2 <sicou2@gmail.com>
2020-04-01 17:26:18 -0500

    Continued and am almost done with Ch 6 (two poms), Started the review document (one pom)

--- python_work/part1/ch06/alien.py
+++ python_work/part1/ch06/alien.py
@@ -60,3 +60,42 @@ alien_0 = {'color': 'green', 'points': 5}
 
 point_value = alien_0.get('point', 'No point value assigned.')
 print(point_value)
+
+print("\nNEXT")
+
+alien_0 = {'color': 'green', 'points': '5'}
+alien_1 = {'color': 'yellow', 'points': '10'}
+alien_2 = {'color': 'red', 'points': '15'}
+
+aliens = [alien_0, alien_1, alien_2]
+
+for alien in aliens:
+    print(alien)
+
+print("\nNEXT")
+
+# Make an empty list for storing aliens.
+aliens = []
+
+# Make 30 green aliens
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+
+for alien in aliens[:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['points'] = 10
+        alien['speed'] = 'medium'
+    elif alien['color'] == 'yellow':
+        alien['color'] = 'red'
+        alien['points'] = 15
+        alien['speed'] = 'fast'
+# Show the first 5 aliens
+for alien in aliens[:5]:
+    print(alien)
+    print("...")
+
+# Show how many aliens have been created.
+print(f"Total number of aliens: {len(aliens)}")
--- python_work/part1/ch06/favorite_language.py
+++ python_work/part1/ch06/favorite_language.py
@@ -59,3 +59,33 @@ print("NEXT")
 print("SET")
 languages = {'python', 'ruby', 'c', 'python'}
 print(languages)
+
+print("NEXT 6-6")
+
+poll_list = ['jen', 'scott', 'sarah', 'daniel', 'edward', 'phil', 
+             'rebeka',]
+
+for name in poll_list:
+    if name in favorite_languages:
+        print(f'Thanks for taking our poll {name.title()}!')
+    else:
+        print(f'Please take our poll {name.title()}!')
+
+print('\nNEXT')
+
+favorite_languages = {
+    'jen': ['python', 'ruby'],
+    'sarah': ['c'],
+    'edward': ['ruby', 'go'],
+    'phil': ['python', 'haskell'],
+    }
+
+for name, languages in favorite_languages.items():
+    if len(languages) == 1:
+        print(f"\n{name.title()}'s favorite language is:")
+        for language in languages:
+            print(f"\t{language.title()}.")        
+    else:
+        print(f"\n{name.title()}'s favorite languages are:")
+        for language in languages:
+            print(f"\t{language.title()}.")
