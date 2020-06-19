5c5c003c7ec6a83fe472420c13758ed785b4ab5a
sicou2 <sicou2@gmail.com>
2020-06-18 19:45:09 -0500

    continue work on learning log and its DIY's

---
+++ python_work/part2/pizzeria/pizzas/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Pizza(models.Model):
+    """A class for collecting different types of Pizza."""
+    name = models.CharField(max_length=200)
+    data_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.name
+
+
+class Topping(models.Model):
+    """A class for the toppings for different pizzas."""
+    pizza = models.ForeignKey(Pizza, on_delete=models.DO_NOTHING)
+    name = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return f"{self.name}, Pizza: {self.pizza}"
